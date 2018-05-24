4a338aebd67fbc0bb9d69f648e7bb0126c07d764
nomyard <nomyards@gmail.com>
2018-05-24 12:54:16 -0400

    some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Ray Rosario, Code Slinger, Web Builder, Cool dude',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
