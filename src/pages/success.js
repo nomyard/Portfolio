58cdc5e326631844b238c6aff2e5d22c3582fa42
nomyard <nomyards@gmail.com>
2018-05-29 14:02:26 -0400

    updated coloring -forgot a page-

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,8 +4,8 @@ import officeGif from '../resources/images/michaelscottdwight.webp'
 const Success = () => (
   <div>
     <img style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '40%'}} src={officeGif} />
-    <h3 style={{textAlign: 'center'}}> You made Michael and Dwight happy, so now I'm happy </h3>
-    <h4 style={{textAlign: 'center'}}> (I'll get back to you ASAP) </h4>
+    <h3 style={{textAlign: 'center', color: 'black'}}> You made Michael and Dwight happy, so now I'm happy </h3>
+    <h4 style={{textAlign: 'center', color: 'black'}}> (I'll get back to you ASAP) </h4>
     
   </div>
 )
